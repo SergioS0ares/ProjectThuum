4f05a4733b2321e40d5b4e820aee59bbefba0964
Julio Yushi Kawahata <104476454+Salahil@users.noreply.github.com>
2024-03-27 20:58:25 -0300

    serviços da task atualizados atualizei a Service da task com novos metodos tirados da repository, o que tava faltando já faz um tempinho.

--- Projeto Thuum/main/src/main/java/com/thuum/main/Spring/SERVICE/TaskService.java
+++ Projeto Thuum/main/src/main/java/com/thuum/main/Spring/SERVICE/TaskService.java
@@ -2,6 +2,7 @@ package com.thuum.main.Spring.SERVICE;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,4 +32,63 @@ public class TaskService {
         return tasks;
     }
     
+    public List<TaskModel> findAllByDataCriada(String data) {
+    	List<TaskModel> tasks = new ArrayList<>();
+    	List<TaskModel> allTasks = repo.findAll();
+    	for (TaskModel task : allTasks) {
+    		String taskIdString = task.getDataCriacao() + "";
+    		String[] taskIdArray = taskIdString.split(",");
+    		for (String taskIdStr : taskIdArray) {
+    			UUID taskid = UUID.fromString(taskIdStr.trim());
+    			tasks.addAll(repo.findAllByTaskId(taskid));
+    		}
+    	}
+    	return tasks;
+    }
+    
+    public List<TaskModel> findAllByDataFinalizada(String data){
+    	List<TaskModel> tasks = new ArrayList<>();
+    	List<TaskModel> allTasks = repo.findAll();
+    	for (TaskModel task : allTasks) {
+    		String taskIdString = task.getDataConclusao() + "";
+    		String[] taskIdArray = taskIdString.split(",");
+    		for (String taskIdStr : taskIdArray) {
+    			UUID taskId = UUID.fromString(taskIdStr.trim());
+    			tasks.addAll(repo.findAllByTaskId(taskId));
+    		}
+    	}
+    	return tasks;
+    }
+    
+    public List<TaskModel> findALLByDataUltimaAtualizacao(String data){
+    	List<TaskModel> tasks = new ArrayList<>();
+    	List<TaskModel> allTasks = repo.findAll();
+    	for (TaskModel task : allTasks) {
+    		String taskidString = task.getDataAlteracao() + "";
+    		String[] taskIdArray = taskidString.split(",");
+    		for (String taskIdStr : taskIdArray) {
+    			UUID taskId = UUID.fromString(taskIdStr.trim());
+    			tasks.addAll(repo.findAllByTaskId(taskId));
+    		}
+    	}
+    	return tasks;
+    }
+    
+    public List<TaskModel> findAllByNome(String nome) {
+    	Optional<TaskModel> treco;
+    	List<TaskModel> tasks = new ArrayList<>();
+    	List<TaskModel> allTasks = repo.findAll();
+    	for (TaskModel task : allTasks) {
+    		String taskIdString = task.getNome();
+    		String[] taskIdArray = taskIdString.split(",");
+    		for (String taskIdStr : taskIdArray) {
+    			UUID taskId = UUID.fromString(taskIdStr.trim());
+    			treco = repo.findById(taskId);
+    			if (treco.get().getNome().contains(nome)) {
+    				tasks.addAll(repo.findAllByTaskId(taskId));
+    			}
+    		}
+    	}
+    	return tasks;
+    }
 }
